eb9187461bfe7b868794083a9da49743964b5674
Aleksandar Gekov <alexander.gekov00@gmail.com>
2023-01-18 00:19:55 +0100

    life simulator dapp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,45 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, ThirdwebNftMedia, useAddress, useContract, useOwnedNFTs, Web3Button } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
+import Confetti from 'react-confetti'
+import useWindowSize from 'react-use/lib/useWindowSize'
 
 const Home: NextPage = () => {
+  // This is the address of our Life Simulator contract. You can find it in the Thirdweb dashboard.
+  const {contract} = useContract("0x09E25aE9b62d8A3221694964fc8D13Ff9fdab4cA");
+
+  // This is the address of the currently connected user's wallet.
+  const address = useAddress();
+
+  // This is a list of all the NFTs owned by the currently connected user.
+  const { data: nfts } = useOwnedNFTs(contract, address);
+
+  // This is the Elder NFT. We'll use it to show a confetti animation when the user owns it.
+  const elderOwned = nfts?.find(nft => nft.metadata.id === "3");
+
+  const { width, height } = useWindowSize()
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
-        </p>
-
+        {/* Confetti animation if we own the Elder NFT */}
+        {elderOwned ? <Confetti width={width} height={height} /> : null}
+        {/* Connect Wallet button */}
         <div className={styles.connect}>
           <ConnectWallet />
         </div>
-
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
+        {elderOwned ? <div className={styles.celebrateText}>CONGRATULATIONS! YOU WON!</div> : null}
+        {/* The Grid displaying our NFTs */}
+        <div className={styles.grid}>{nfts?.map(nft => <div className={styles.nft} key={nft.metadata.id.toString()}>
+          <ThirdwebNftMedia width="300"  metadata={nft.metadata} />
+          {nft.metadata.name}
+          <div>({nft.quantityOwned})</div>
+          </div>)}</div>
+        {/* Button for claiming a Baby NFT */}
+        <Web3Button contractAddress="0x09E25aE9b62d8A3221694964fc8D13Ff9fdab4cA" action={contract => contract.erc1155.claim(0,1)}>Claim a Baby</Web3Button>
+        <hr style={{margin: 10}} />
+        {/* Button for aging our human */}
+        <Web3Button accentColor="blue" contractAddress="0x09E25aE9b62d8A3221694964fc8D13Ff9fdab4cA" action={contract => contract.call("age")}>Age 💀</Web3Button>
       </main>
     </div>
   );
